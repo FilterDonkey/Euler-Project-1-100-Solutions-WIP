ab17a1b706b0b33a106f1c7044252a80d60509c5
FilterDonkey <renstrmerik@gmail.com>
2022-10-25 17:47:44 +0200

    Reworked prime-finding function to work from 0

--- Euler7.py
+++ Euler7.py
@@ -5,20 +5,22 @@
 # We simply create a function to check if a number is prime
 def prime_q(x):
     poss_prime = False
-    for y in range(2,x-1):
-        result = x % y
-        if result == 0:
-            poss_prime = False
-            break
-        else:
-            poss_prime = True
+    if x > 4:
+        for y in range(2,x-1):
+            result = x % y
+            if result == 0:
+                poss_prime = False
+                break
+            else:
+                poss_prime = True
+    if x > 1 and x < 4:
+        poss_prime = True
     return poss_prime
 
-primes = 2 # Number of primes found so far, the function does not work when calling it with 2 or 3 so I've counted those
-# when initializing.
-prime_num = 3  # Initialize the number we check each iteration, skipping 2 & 3
+primes = 0 # Number of primes found so far
+prime_num = 0  # Initialize the number we check each iteration, skipping 2 & 3
 while primes < 10001:
-    prime_num = prime_num + 2
+    prime_num = prime_num + 1
 
     if prime_q(prime_num) == True:
         primes = primes + 1
